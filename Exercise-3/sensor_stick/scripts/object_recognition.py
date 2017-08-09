324ba02ebf8d12a0b049bfb9a7ddcb8691963c2a
Ketan Kharche <ketan3393@gmail.com>
2017-08-08 21:01:13 -0700

    fix: Adjust passthrough filter to remove table edge from cluster

--- Exercise-3/sensor_stick/scripts/object_recognition.py
+++ Exercise-3/sensor_stick/scripts/object_recognition.py
@@ -41,7 +41,7 @@ def pcl_callback(pcl_msg):
     passthrough=cloud_vox.make_passthrough_filter()
     filter_axis='z'
     passthrough.set_filter_field_name (filter_axis)
-    axis_min = 0.6
+    axis_min = 0.77
     axis_max = 1.1
     passthrough.set_filter_limits (axis_min, axis_max)
     cloud_passthrough = passthrough.filter()
